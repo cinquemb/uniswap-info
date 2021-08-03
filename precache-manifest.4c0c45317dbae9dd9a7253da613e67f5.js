2ef8feca334816d1742ca84bdc805128b914c59e
cinquemb <1519017+cinquemb@users.noreply.github.com>
2021-08-03 05:24:41 -0400

    GH pages

---
+++ precache-manifest.4c0c45317dbae9dd9a7253da613e67f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ac3b760a1a9c0d955c299912da7a7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57bd0fee1e323fa71098",
+    "url": "./static/js/2.74b2fd4c.chunk.js"
+  },
+  {
+    "revision": "d255601c39ba9ea8be3f576f2fb7e1d5",
+    "url": "./static/js/2.74b2fd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8556939ba0cbd5001c27",
+    "url": "./static/js/main.799b8102.chunk.js"
+  },
+  {
+    "revision": "357ac4831e8a43876885",
+    "url": "./static/js/runtime-main.18f2d56a.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "./static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "./static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "./static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "./static/media/wordmark_white.9914390f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ac4214667aa2596f5cbfd9380d0784f.js"
+  "./precache-manifest.4c0c45317dbae9dd9a7253da613e67f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
